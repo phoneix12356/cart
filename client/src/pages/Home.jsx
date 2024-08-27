9fcce6c638064e0740845977d9cd527b38d61867
phoneix12356 <95468972+phoneix12356@users.noreply.github.com>
2024-08-27 19:16:14 +0530

    11th commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -42,7 +42,7 @@ function Home() {
   return (
     <div className="container mx-auto mt-8 max-w-6xl">
       <h1 className="text-3xl font-bold mb-4">Products</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product) => (
           <div
             key={product.id}
